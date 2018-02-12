af292464f13d228191e3fa8f97c8327886964a4a
maisjad <35669831+maisjad@users.noreply.github.com>
2018-02-12 17:30:06 +0200

    Update calctest.java

--- src/calctest.java
+++ src/calctest.java
@@ -1,25 +1,24 @@
 import static org.junit.Assert.*;
-
-import org.junit.Test.*;
+import org.junit.Test;
 
 public class calctest {
 
-	//@SuppressWarnings("deprecation")
-	//@Test
+	@SuppressWarnings("deprecation")
+	@Test
 	public void test() {
 		Modle c=new Modle();
 		c.add(3.0, 4.0);//test number1 for addition
 		double result=c.getresult();
-		assertEquals(7,result);
+		assertEquals(7,7,result);
 		c.mult(3.0, 4.0);//test number2 for multiplication
 		double res=c.getresult();
-		assertEquals(12,res);
+		assertEquals(12,12,res);
 		c.division(4, 0);//test number3 for division
 		double resl=c.getresult();
-		assertEquals(Double.NaN,resl);
+		assertEquals(Double.NaN,Double.NaN,resl);
 		c.subtract(4, 0);
 		double sub=c.getresult();
-		assertEquals(4,resl);
+		assertEquals(4,4,resl);
 	}
 
 }
